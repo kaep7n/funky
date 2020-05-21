14c799b4acafff7444637c154b5710858ec17261
kaeptn <kaeptn@outlook.com>
2020-05-21 10:33:55 +0200

    playground

---
+++ src/Funky.Playground.Kafka.Producer/Program.cs
@@ -0,0 +1,63 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace Funky.Playground.Kafka.Producer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var input = string.Empty;
+
+            while (input != "exit")
+            {
+                await AwaitDelivery();
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static async Task AwaitDelivery()
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            // If serializers are not specified, default serializers from
+            // `Confluent.Kafka.Serializers` will be automatically used where
+            // available. Note: by default strings are encoded as UTF8.
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = "test" });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+
+        private static void FlushDelivery(string[] args)
+        {
+            var conf = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            Action<DeliveryReport<Null, string>> handler = r =>
+                Console.WriteLine(!r.Error.IsError
+                    ? $"Delivered message to {r.TopicPartitionOffset}"
+                    : $"Delivery Error: {r.Error.Reason}");
+
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
+            {
+                for (int i = 0; i < 100; ++i)
+                {
+                    p.Produce("my-topic", new Message<Null, string> { Value = i.ToString() }, handler);
+                }
+
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
