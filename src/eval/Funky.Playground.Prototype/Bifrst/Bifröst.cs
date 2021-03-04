fa30b17b3a96a3c21476004dacf354bfb893f16f
kaeptn <kaeptn@outlook.com>
2021-03-04 10:32:31 +0100

    bifröst

---
+++ src/eval/Funky.Playground.Prototype/Bifrst/Bifröst.cs
@@ -0,0 +1,99 @@
+﻿using Bifrst;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Funky.Playground.Prototype.Bifrst
+{
+    public class Bifröst
+    {
+        private readonly ConcurrentDictionary<string, TopicStream> topicStreams = new();
+        private readonly List<ISubscription> subscribers = new();
+
+        public Bifröst()
+        {
+        }
+
+        public async ValueTask PublishAsync(string topic, object payload)
+            => await this.PublishAsync(topic, payload, new TopicOptions(Capacity: null));
+
+        public async ValueTask PublishAsync(string topic, object payload, TopicOptions options)
+        {
+            var topicStream = this.topicStreams.GetOrAdd(topic, (key) =>
+            {
+                var topicStream = new TopicStream(key, options);
+
+                Task.Run(async () =>
+                {
+                    await foreach (var message in topicStream.ReadAllAsync())
+                    {
+                        var subs = this.subscribers.Where(t => Regex.IsMatch(topicStream.Key, t.Pattern));
+
+                        foreach (var subGroup in subs.GroupBy(s => s.Group))
+                        {
+                            foreach (var sub in subGroup)
+                            {
+                                try
+                                {
+                                    await sub.WriteAsync(message);
+                                    break;
+                                }
+                                catch (Exception)
+                                {
+                                }
+                            }
+                        }
+                    }
+                });
+
+                return topicStream;
+            });
+
+            await topicStream.WriteAsync(payload);
+        }
+
+        public ValueTask SubscribeAsync(ISubscription subscription)
+        {
+            this.subscribers.Add(subscription);
+
+            return ValueTask.CompletedTask;
+        }
+
+        public ValueTask UnsubscribeAsync(ISubscription subscription)
+        {
+            this.subscribers.Remove(subscription);
+
+            return ValueTask.CompletedTask;
+        }
+    }
+
+    public interface ISubscription
+    {
+        public string Pattern { get; }
+
+        public string Group { get; }
+
+        ValueTask WriteAsync(object message);
+    }
+
+    public class Subscription : ISubscription
+    {
+        private readonly Channel<object> stream = Channel.CreateUnbounded<object>();
+
+        public Subscription(string pattern, string group)
+        {
+            this.Pattern = pattern;
+            this.Group = group;
+        }
+
+        public string Pattern { get; }
+
+        public string Group { get; }
+
+        public async ValueTask WriteAsync(object message) => await this.stream.Writer.WriteAsync(message);
+    }
+}
---
+++ src/eval/Funky.Playground.Prototype/Bifrst/TopicStream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Bifrst
+{
+    public record Message(Guid Id, long Offset, object Payload);
+
+    public record TopicOptions(int? Capacity);
+
+    internal class TopicStream
+    {
+        private readonly Channel<Message> stream;
+        private long offset = 0;
+
+        public TopicStream(string key, TopicOptions options)
+        {
+            this.Key = key;
+
+            this.stream = !options.Capacity.HasValue
+                ? Channel.CreateUnbounded<Message>()
+                : Channel.CreateBounded<Message>(options.Capacity.Value);
+        }
+
+        public async ValueTask WriteAsync(object payload)
+        {
+            await this.stream.Writer.WriteAsync(new Message(Guid.NewGuid(), this.offset, payload));
+            this.offset++;
+        }
+
+        public string Key { get; }
+
+        internal IAsyncEnumerable<object> ReadAllAsync()
+            => this.stream.Reader.ReadAllAsync();
+    }
+}
